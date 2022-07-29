1d744f250167b206fd768ca5d239b599a497f08e
Tomi_Ohl <ohlslager.tom@gmail.com>
2022-07-29 16:16:50 +0200

    Deploy script: pad bytes32 from the right

--- scripts/deploy-distributor.ts
+++ scripts/deploy-distributor.ts
@@ -9,7 +9,7 @@ const rewardedRole = 1904; // The role that will be able to claim rewards. (defa
 // ORACLE CONFIG (default: Rinkeby)
 const chainlinkToken = "0x01BE23585060835E02B77ef475b0Cc51aA1e0709";
 const oracleAddress = "0x188b71C9d27cDeE01B9b0dfF5C1aff62E8D6F434";
-const jobId = ethers.utils.hexZeroPad("0xa56c23c069b446a5bfd3b5fc91383991", 32);
+const jobId = "0xa56c23c069b446a5bfd3b5fc91383991".padEnd(66, "0");
 const oracleFee = ethers.BigNumber.from("50000000000000000");
 
 async function main() {
@@ -24,6 +24,8 @@ async function main() {
     jobId,
     oracleFee
   );
+
+  console.log("Deploying contract...");
 
   await distributor.deployed();
 
