44af244420c118527c5f9e6fb103f72cf1f8a312
Tomi_Ohl <ohlslager.tom@gmail.com>
2023-03-02 01:09:36 +0100

    Update deploy scripts

--- scripts/deploy-distributor.ts
+++ scripts/deploy-distributor.ts
@@ -5,13 +5,13 @@ const token = "0x..."; // The address of the token to distribute.
 const amount = ethers.utils.parseEther("1"); // The amount of tokens each address can claim.
 const distributionDuration = 86400; // The length of the distribution period in seconds.
 const guildId = "1985"; // The id of the guild the rewarded role is in. (default: Our Guild)
-const rewardedRole = "1904"; // The role that will be able to claim rewards. (default: Guild Member)
+const rewardedRole = "1904"; // The role that will be able to claim rewards. (default: Explorer)
 
-// ORACLE CONFIG (default: Rinkeby)
-const chainlinkToken = "0x01BE23585060835E02B77ef475b0Cc51aA1e0709";
+// ORACLE CONFIG (default: Goerli)
+const chainlinkToken = "0x326C977E6efc84E512bB9C30f76E30c160eD06FB";
 const oracleAddress = "0x188b71C9d27cDeE01B9b0dfF5C1aff62E8D6F434";
 const jobId = "0x7599d3c8f31e4ce78ad2b790cbcfc673".padEnd(66, "0");
-const oracleFee = ethers.BigNumber.from("50000000000000000");
+const oracleFee = ethers.utils.parseEther("0.05");
 
 async function main() {
   const GatedDistributor = await ethers.getContractFactory("GatedDistributor");
--- scripts/deploy-nft.ts
+++ scripts/deploy-nft.ts
@@ -8,13 +8,13 @@ const maxSupply = 500; // The maximum amount of tokens that can ever get minted.
 
 // AIRDROP CONFIG
 const guildId = "1985"; // The id of the guild the rewarded role is in. (default: Our Guild)
-const rewardedRole = "1904"; // The role that will be able to claim rewards. (default: Guild Member)
+const rewardedRole = "1904"; // The role that will be able to claim rewards. (default: Explorer)
 
-// ORACLE CONFIG (default: Rinkeby)
-const chainlinkToken = "0x01BE23585060835E02B77ef475b0Cc51aA1e0709";
+// ORACLE CONFIG (default: Goerli)
+const chainlinkToken = "0x326C977E6efc84E512bB9C30f76E30c160eD06FB";
 const oracleAddress = "0x188b71C9d27cDeE01B9b0dfF5C1aff62E8D6F434";
 const jobId = "0x7599d3c8f31e4ce78ad2b790cbcfc673".padEnd(66, "0");
-const oracleFee = ethers.BigNumber.from("50000000000000000");
+const oracleFee = ethers.utils.parseEther("0.05");
 
 async function main() {
   const GatedERC721 = await ethers.getContractFactory("GatedERC721");
